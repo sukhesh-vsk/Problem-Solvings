8f053babedeae9420e11c311845551c6f8104cb9
sukhesh-vsk <sukhesh.vsk2005@gmail.com>
2023-11-30 23:59:41 +0530

    Min Operations Bits

--- JAVA/MinOperationsBits.java
+++ JAVA/MinOperationsBits.java
@@ -29,6 +29,12 @@ Explanation: The binary representation of 6 is "110".
 
 class Solution {
     public int minimumOneBitOperations(int n) {
-        
+        n ^= n>>16;
+        n ^= n>>8;
+        n ^= n>>4;
+        n ^= n>>2;
+        n ^= n>>1;
+
+        return n;
     }
 }
