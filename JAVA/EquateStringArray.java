3f93a8f6d5a866222894cea04f35fed6d3b1cbc1
sukhesh-vsk <sukhesh.vsk2005@gmail.com>
2023-12-01 16:12:12 +0530

    Problem-1662-Code-Updated

--- JAVA/EquateStringArray.java
+++ JAVA/EquateStringArray.java
@@ -19,7 +19,7 @@ Output: false
 
 */
 
-class Solution {
+class EquateStringArray {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         return String.join("", word1).equals(String.join("", word2));
     }
