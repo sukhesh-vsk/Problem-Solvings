f80d9b8c04a56b69baf36403684956023a16cc84
sukhesh-vsk <sukhesh.vsk2005@gmail.com>
2024-02-04 00:30:04 +0530

    Partition Array for Maximum Sum - LC

---
+++ Leetcode/PartitionArrayMaxSum.py
@@ -0,0 +1,49 @@
+'''
+1043. Partition Array for Maximum Sum
+
+Given an integer array arr, partition the array into (contiguous) subarrays of length at most k. 
+After partitioning, each subarray has their values changed to become the maximum value of that subarray.
+Return the largest sum of the given array after partitioning. Test cases are generated so that the answer fits in a 32-bit integer.
+
+Example 1:
+Input: arr = [1,15,7,9,2,5,10], k = 3
+Output: 84
+Explanation: arr becomes [15,15,15,9,10,10,10]
+
+Example 2:
+Input: arr = [1,4,1,5,7,3,6,1,9,9,3], k = 4
+Output: 83
+
+Example 3:
+Input: arr = [1], k = 1
+Output: 1
+ 
+
+Constraints:
+    1 <= arr.length <= 500
+    0 <= arr[i] <= 109
+    1 <= k <= arr.length
+'''
+
+class Solution:
+    def __init__(self):
+        self.memo = {}
+
+    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        return self.solve(arr, k, 0, len(arr))
+
+    def solve(self, arr, k, idx, n):
+        if idx >= n:
+            return 0
+        
+        if idx in self.memo:
+            return self.memo[idx]
+
+        mx = ans = 0
+        for i in range(idx, min(idx+k, n)):
+            mx = max(mx, arr[i])
+            ans = max(ans, (i - idx + 1) * mx + self.solve(arr, k, i+1, n))
+
+        self.memo[idx] = ans
+
+        return ans
