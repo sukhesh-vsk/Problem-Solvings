ef7932f17f315b7f27cd8b0c210d5c4c643bf0c8
sukhesh-vsk <sukhesh.vsk2005@gmail.com>
2024-02-24 23:59:15 +0530

    FInd All People - LC

---
+++ Leetcode/PeopleWithSecret.java
@@ -0,0 +1,28 @@
+/*
+2092. Find All People With Secret
+
+You are given an integer n indicating there are n people numbered from 0 to n - 1. You are also given a 0-indexed 2D integer array meetings where meetings[i] = [xi, yi, timei] indicates that person xi and person yi have a meeting at timei. A person may attend multiple meetings at the same time. Finally, you are given an integer firstPerson.
+
+Person 0 has a secret and initially shares the secret with a person firstPerson at time 0. This secret is then shared every time a meeting takes place with a person that has the secret. More formally, for every meeting, if a person xi has the secret at timei, then they will share the secret with person yi, and vice versa.
+
+The secrets are shared instantaneously. That is, a person may receive the secret and share it with people in other meetings within the same time frame.
+
+Return a list of all the people that have the secret after all the meetings have taken place. You may return the answer in any order.
+
+Example 1:
+Input: n = 6, meetings = [[1,2,5],[2,3,8],[1,5,10]], firstPerson = 1
+Output: [0,1,2,3,5]
+Explanation:
+At time 0, person 0 shares the secret with person 1.
+At time 5, person 1 shares the secret with person 2.
+At time 8, person 2 shares the secret with person 3.
+At time 10, person 1 shares the secret with person 5.​​​​
+Thus, people 0, 1, 2, 3, and 5 know the secret after all the meetings.
+*/
+
+
+class Solution {
+    public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
+        
+    }
+}
