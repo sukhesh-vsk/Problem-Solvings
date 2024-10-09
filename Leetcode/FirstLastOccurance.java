09b506e305e4607d1f135446a3a70d44d7816bdb
sukhesh-vsk <115344170+sukhesh-vsk@users.noreply.github.com>
2024-10-10 00:01:27 +0530

    add cpp version of first last occurance

--- Leetcode/FirstLastOccurance.java
+++ Leetcode/FirstLastOccurance.java
@@ -20,6 +20,52 @@ Output: [-1,-1]
 
 */
 
+//CPP 
+#include <iostream>
+
+using namespace std;
+
+int binsearch(int*, int, int, int, bool);
+
+int main() {
+    int n;
+    cin >> n;
+    int books[n];
+    for(int i=0; i<n; i++)
+        cin >> books[i];
+    int key;
+    cin >> key;
+    
+    int st = binsearch(books, key, 0, n-1, false);
+    
+    if(st==-1) {
+        cout << "NO OCCURRENCES";
+        return 0;
+    }
+    
+    int en = binsearch(books, key, st+1, n-1, true);
+    cout << st << " " << ((en == -1) ? st : en);
+}
+
+int binsearch(int* arr, int key, int st, int en, bool isEND) {
+  if(isEND) cout << "Hello\n";
+    if(st>en) return -1;
+    int mid = st + (en-st)/2;
+    if(arr[mid] == key){
+      int next;
+      if (isEND)
+        next = binsearch(arr, key, mid+1, en, isEND);
+      else
+        next = binsearch(arr, key, st, mid-1, isEND);
+      return (next != -1) ? next : mid;
+    }
+    
+    if(arr[mid] < key)
+        return binsearch(arr, key, mid+1, en, isEND);
+    return binsearch(arr, key, st, mid-1, isEND);
+}
+
+// JAVA
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int first = leftOccurance(nums, 0, nums.length-1, target);
